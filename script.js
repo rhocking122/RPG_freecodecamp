1cea1c78b808c3c0ae3c2d23dd60875f224d67a2
Richard Hocking <141461457+rhocking122@users.noreply.github.com>
2024-06-22 14:04:06 -0500

    step 41

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
 }
 function goCave() {}
 function fightDragon(){}
